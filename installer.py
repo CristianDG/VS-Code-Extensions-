a46c75bace37dc14229c445a893f76ba1677626a
CristianDG <dgcristian@hotmail.com>
2019-12-20 00:05:05 +0100

    adding an installer

--- installer.py
+++ installer.py
@@ -5,5 +5,3 @@ with open('extensions.txt','r') as f:
     lines = [line.split('\n')[0] for line in f.readlines()]
     for line in lines:
         subprocess.call(['code', '--install-extension', line])
-
-print(lines)
